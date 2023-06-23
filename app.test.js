c19f0c7d654841ee9092b827eb8e00e998fc9a8b
Juntao Qiu <juntao.qiu@gmail.com>
2023-06-23 11:57:03 +1000

    update steps for github actions

--- app.test.js
+++ app.test.js
@@ -2,8 +2,8 @@ import request from "supertest"
 import app from "./app.js"
 
 describe('get quotes', () => {
-    it("should respond with a 200 status code", async () => {
-        const response = await request(app).get("/quotes")
-        expect(response.statusCode).toBe(200)
-      })
+  it("should respond with a 200 status code", async () => {
+    const response = await request(app).get("/quotes")
+    expect(response.statusCode).toBe(200)
+  })
 })
